a0aa85a07ea482bd9a735fec449305411a90f8fc
Blaire Baker <bakerblaire@gmail.com>
2020-12-09 19:59:20 -0600

    commit tree dc368dd4e96f3856eee1d0563b8ca5b7b8e2c0d2

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,24 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(id: 0)
+    @recipe.ingredients.build(id: 1)
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title, 
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
+  end
+
 end
